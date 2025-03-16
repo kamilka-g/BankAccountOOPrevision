c4ce0f8967874b3c37cf97a61b92dc4d01dcd4c4
Kamila Matusik <s4charyna@gmail.com>
2025-03-16 20:58:28 +0100

    Base of program

--- BankAccountOOPrevision/Program.cs
+++ BankAccountOOPrevision/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount bankAccount = new BankAccount();
+            
         }
     }
 }
