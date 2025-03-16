50dc8edc8bdaf3d002a8ac1aea4d5fd2f57ec02e
Kamila Matusik <s4charyna@gmail.com>
2025-03-16 20:54:57 +0100

    Add project files.

---
+++ BankAccountOOPrevision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccountOOPrevision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
